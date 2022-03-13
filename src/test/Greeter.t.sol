91d08879ee96132c954f5e4d20ff183c0a281477
steven <steven.nevins.js@gmail.com>
2022-03-12 20:15:48 -0500

    remove fixtures

--- src/test/Greeter.t.sol
+++ src/test/Greeter.t.sol
@@ -1,33 +1,42 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import {Fixture1, Fixture2} from "./fixtures/Fixture2.sol";
+import "ds-test/test.sol";
+import "forge-std/Vm.sol";
 import "log/console.sol";
+import {Greeter, GreeterBS, Blacksmith} from "./blacksmith/Greeter.bs.sol";
 import {Errors} from "../Greeter.sol";
 
-contract Greet is Fixture1 {
-    function testCannotGm() public {
-        try alice.greeter.greet("gm") {
-            fail();
-        } catch Error(string memory error) {
-            assertEq(error, Errors.CannotGm);
-        }
+contract GreetTest is DSTest {
+    Vm internal constant hevm = Vm(HEVM_ADDRESS);
+    // contracts
+    Greeter internal greeter;
+    struct User {
+        address addr; // to avoid external call, we save it in the struct
+        Blacksmith base;
+        GreeterBS greeter;
+    }
+    User alice;
+    User bob;
+
+    function createUser(address _addr, uint256 _privateKey)
+        public
+        virtual
+        returns (User memory)
+    {
+        Blacksmith base = new Blacksmith(_addr, _privateKey);
+        GreeterBS _greet = new GreeterBS(_addr, _privateKey, address(greeter));
+        base.deal(100 ether);
+        return User(base.addr(), base, _greet);
     }
 
-    function testCanSetGreeting() public {
-        alice.greeter.greet("hi");
-        console.log(greeter.greeting());
-        assertEq(greeter.greeting(), "hi");
+    function setUp() public virtual {
+        greeter = new Greeter();
+        alice = createUser(address(0), 111);
+        bob = createUser(address(1), 123);
+        greeter.transferOwnership(alice.addr);
     }
 
-    function testWorksForAllGreetings(string memory greeting) public {
-        alice.greeter.greet(greeting);
-        console.log(greeter.greeting());
-        assertEq(greeter.greeting(), greeting);
-    }
-}
-
-contract Greet2 is Fixture2 {
     function testCannotGm() public {
         try alice.greeter.greet("gm") {
             fail();
