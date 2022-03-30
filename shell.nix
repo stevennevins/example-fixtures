eaaad02aca0eadc07647f7797e4b7c2e7fd8b711
steven <steven.nevins.js@gmail.com>
2022-03-30 12:46:58 -0400

    change back to shell

--- shell.nix
+++ shell.nix
@@ -1,18 +1,17 @@
+
+   
 let
   pkgs = import (builtins.fetchGit rec {
     name = "dapptools-${rev}";
     url = https://github.com/dapphub/dapptools;
-    rev = "5bca0569dc1b0a4ade46fbe53d0ba4b3c0ac4ca7";
+    rev = "fb9476ded759da44c449eb391cc67bfb0df61112";
   }) {};
 
 in
   pkgs.mkShell {
     src = null;
-    name = "dapptools-template";
+    name = "rari-capital-solmate";
     buildInputs = with pkgs; [
       pkgs.dapp
-      pkgs.seth
-      pkgs.go-ethereum-unlimited
-      pkgs.hevm
     ];
   }
